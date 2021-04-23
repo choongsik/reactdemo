2dc5df2ee5a16c3628f1db5dcb040fa6327ee3ec
choongsik <charles77.kim@gmail.com>
2021-04-23 10:06:18 +0900

    bodyparser 이용

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const mongoose = require("mongoose");
 mongoose
   .connect(
@@ -20,6 +24,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!adddddddd");
 });
 
+app.post("/register", (req, res) => {
+  //회원 가입 정보를 Client에서 받아서 DB에 넣기
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
